ccb6a2a171d386af7d6ece438f4002a3e90661f7
davidpillco <davidpilco@gmail.com>
2021-12-05 12:39:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tallergps3/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.tallergps3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tallergps3.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+// importaciones gps
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    // Variable de ubicacion
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    val PERMISSION_ID = 42
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Preguntar si se tiene permisos.
+        if (allPermissionsGrantedGPS()){
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        } else {
+            // Si no hay permisos solicitarlos al usuario.
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID)
+        }
+
+        binding.btndetectar.setOnClickListener {
+            leerubicacionactual()
+        }
+    }
+    private fun allPermissionsGrantedGPS() = REQUIRED_PERMISSIONS_GPS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun leerubicacionactual(){
+        if (checkPermissions()){
+            if (isLocationEnabled()){
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.lastLocation.addOnCompleteListener(this){ task ->
+                        var location: Location? = task.result
+                        if (location == null){
+                            requestNewLocationData()
+                        } else {
+                            binding.lbllatitud.text = "LATITUD = " + location.latitude.toString()
+                            binding.lbllongitud.text = "LONGITUD = " + location.longitude.toString()
+
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Activar ubicación", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+                this.finish()
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID)
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData(){
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallBack, Looper.myLooper())
+    }
+
+    private val mLocationCallBack = object : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation : Location = locationResult.lastLocation
+            binding.lbllatitud.text = "LATITUD = " + mLastLocation.latitude.toString()
+            binding.lbllongitud.text = "LONGITUD = "+ mLastLocation.longitude.toString()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+    companion object {
+        private val REQUIRED_PERMISSIONS_GPS= arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+}
