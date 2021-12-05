15e4b98eaefa8eaf77cb9a29896bdf7d9d943f41
davidpillco <davidpilco@gmail.com>
2021-12-05 18:50:27 -0500

    Implementacion ubicacion

--- app/src/main/java/com/example/tallergps3/MainActivity.kt
+++ app/src/main/java/com/example/tallergps3/MainActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tallergps3.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
-// importaciones gps
+// Importaciones gps
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
 import android.os.Looper
@@ -17,19 +18,23 @@ import android.provider.Settings
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.GoogleMap
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     // Variable de ubicacion
     lateinit var mFusedLocationClient: FusedLocationProviderClient
+    //Determinamos un permiso para la API
     val PERMISSION_ID = 42
+    // Acceder a cualquier vista del proyecto
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Obtener una instancia de la actividad
         binding = ActivityMainBinding.inflate(layoutInflater)
+        // Acceder a todos los componentes de la interfaz grafica - propiedad raiz
         setContentView(binding.root)
         //Preguntar si se tiene permisos.
         if (allPermissionsGrantedGPS()){
@@ -38,29 +43,49 @@ class MainActivity : AppCompatActivity() {
             // Si no hay permisos solicitarlos al usuario.
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID)
         }
-
+        // Ejecución del boton
         binding.btndetectar.setOnClickListener {
             leerubicacionactual()
         }
+
     }
+    // Solicita que la aplicacion necesita de GPS
     private fun allPermissionsGrantedGPS() = REQUIRED_PERMISSIONS_GPS.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun leerubicacionactual(){
+        //Evaluar los permiso de la app
         if (checkPermissions()){
+            // Evaluar activacion de la ubicacion
             if (isLocationEnabled()){
+                // Evaluar los permisos del usuario
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     mFusedLocationClient.lastLocation.addOnCompleteListener(this){ task ->
                         var location: Location? = task.result
                         if (location == null){
                             requestNewLocationData()
                         } else {
+                            // Presentar las coordenadas
                             binding.lbllatitud.text = location.latitude.toString()
                             binding.lbllongitud.text = location.longitude.toString()
+                            // Obtener ubicacion
+                            var cityName = ""
+                            var countryName = ""
+                            var provincia = ""
+                            var postal = ""
+                            // Geocoder - Clase de codificacion geografica
+                            var geoCoder = Geocoder(this, Locale.getDefault())
+                            var Adress = geoCoder.getFromLocation(location.latitude,location.longitude,3)
+                            cityName = Adress.get(0).locality //Ciudad
+                            countryName = Adress.get(0).countryName // Pais
+                            provincia = Adress.get(0).adminArea // Provincia  o Estado
+                            postal = Adress.get(0).postalCode // Código postal
+                            binding.lblUbicacion.text = countryName +", "+ provincia+ ", "+cityName+" - "+ postal
 
                         }
                     }
+
                 }
             } else {
                 Toast.makeText(this, "Activar ubicación", Toast.LENGTH_SHORT).show()
@@ -73,6 +98,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
     @SuppressLint("MissingPermission")
+    // Obtener actualizaciones de ubicacion
     private fun requestNewLocationData(){
         var mLocationRequest = LocationRequest()
         mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
@@ -82,22 +108,20 @@ class MainActivity : AppCompatActivity() {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallBack, Looper.myLooper())
     }
-
+    // Constructor - Evalua cambios en la disponibilidad de los datos de ubicacion
     private val mLocationCallBack = object : LocationCallback(){
         override fun onLocationResult(locationResult: LocationResult) {
             var mLastLocation : Location = locationResult.lastLocation
-            binding.lbllatitud.text = "LATITUD = " + mLastLocation.latitude.toString()
-            binding.lbllongitud.text = "LONGITUD = "+ mLastLocation.longitude.toString()
         }
     }
-
+    // Permiso si el GPS esta habilitada
     private fun isLocationEnabled(): Boolean {
         var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
             LocationManager.NETWORK_PROVIDER
         )
     }
-
+    // Verificar permisos de la aplicacion que fueron implementados
     private fun checkPermissions(): Boolean {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
@@ -106,7 +130,12 @@ class MainActivity : AppCompatActivity() {
         }
         return false
     }
+    // Recibe los permisos
     companion object {
         private val REQUIRED_PERMISSIONS_GPS= arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
     }
+
+
+
 }
+
